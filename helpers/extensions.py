80534df1350e39c11e6b31106cc528ee66565db1
aydnylmz <aydin@marketyo.com>
2021-01-02 20:02:43 +0300

    lower files and read

--- helpers/extensions.py
+++ helpers/extensions.py
@@ -1,27 +1,19 @@
 import glob
 import os
-# import nltk.data
-
-
-def load_multiple_corpus_files(path):
-    read_files = glob.glob(path + '*')
-
-    sub_folder = path.split('/')[-2]
-
-    with open(f"docs/{sub_folder}/{sub_folder}.txt", "wb") as outfile:
-        for f in read_files:
-            with open(f, "rb") as infile:
-                for line in infile:
-                    line = line.lower()
-                    outfile.write(line)
-
-    # nltk_data = nltk.data.load('docs/result', format='raw')
-
-    # delete_file('docs/result')
-
-    # return nltk_data
 
 
 def delete_file(full_path):
     if os.path.exists(full_path):
         os.remove(full_path)
+
+
+def lower_files(parent_path):
+    read_files = glob.glob(parent_path + '*')
+
+    for file in read_files:
+        with open(file, "r") as infile:
+            text = infile.read()
+
+            lines = [text.lower() for line in file]
+            with open(file, 'w') as out:
+                out.writelines(lines)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # %%
 # from helpers.extensions import load_multiple_corpus_files
 from nltk.corpus.reader.tagged import TaggedCorpusReader
+from helpers.extensions import lower_files
 
 # %%
-root = './venv/nltk_data/corpora/brown_hw/Train/'
-reader = TaggedCorpusReader(root, '.*')
+lower_files('./venv/nltk_data/corpora/brown_hw/Train/')
+lower_files('./venv/nltk_data/corpora/brown_hw/Test/')
+
+# %%
+train_root = './venv/nltk_data/corpora/brown_hw/Train/'
+train_reader = TaggedCorpusReader(train_root, '.*')
+
+# %%
+train_reader.words()
+# %%
