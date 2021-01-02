cf667b12ab6dda60ab77d7194122a86192722d3e
aydnylmz <aydin@marketyo.com>
2021-01-02 20:56:46 +0300

    word count of train words

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # %%
 # from helpers.extensions import load_multiple_corpus_files
 from nltk.corpus.reader.tagged import TaggedCorpusReader
+from collections import Counter
+import nltk
 from helpers.extensions import lower_files
 
 # %%
-lower_files('./venv/nltk_data/corpora/brown_hw/Train/')
-lower_files('./venv/nltk_data/corpora/brown_hw/Test/')
+# lower_files('./venv/nltk_data/corpora/brown_hw/Train/')
+# lower_files('./venv/nltk_data/corpora/brown_hw/Test/')
 
 # %%
 train_root = './venv/nltk_data/corpora/brown_hw/Train/'
 train_reader = TaggedCorpusReader(train_root, '.*')
 
 # %%
-train_reader.words()
+train_words = train_reader.words()
+
+# %%
+train_word_counts = Counter(train_words)
+
 # %%
