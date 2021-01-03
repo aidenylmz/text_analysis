1e849ea25839b28767fa9f65450db9ffc212f374
aydnylmz <aydin@marketyo.com>
2021-01-03 20:54:15 +0300

    word count fixed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # %%
 # from helpers.extensions import load_multiple_corpus_files
 from nltk.corpus.reader.tagged import TaggedCorpusReader
-from collections import Counter
 import nltk
+import json
 from helpers.extensions import load_multiple_corpus_files
 
 # %%
@@ -14,12 +14,15 @@ load_multiple_corpus_files('./venv/nltk_data/corpora/brown_hw/Test/')
 
 # %%
 train_root = './docs/Train/'
-train_reader = TaggedCorpusReader(train_root, '.txt')
+train_reader = TaggedCorpusReader(train_root, '.*')
 
 # %%
 train_words = train_reader.words()
 
 # %%
-train_word_counts = Counter(train_words)
+train_word_counts = nltk.FreqDist(train_words)
+# %%
+with open('docs/train_word_counts.json', 'w') as fp:
+    json.dump(train_word_counts, fp)
 
 # %%
