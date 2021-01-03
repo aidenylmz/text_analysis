277eaa701cdc18d3601ab2713144049944112a3b
aidenylmz <aydinyilmaz1205@gmail.com>
2021-01-03 23:02:12 +0300

    replace with unknown failed

--- helpers/extensions.py
+++ helpers/extensions.py
@@ -29,3 +29,79 @@ def load_multiple_corpus_files(path):
                 for line in infile:
                     line = line.lower()
                     outfile.write(line)
+
+
+def sort_dict_by_value(dictionary: dict):
+    return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1])}
+
+
+def count_keys_by_value(dictionary, value=1):
+    res = 0
+    for key in dictionary:
+        if dictionary[key] == value:
+            res = res + 1
+
+    return res
+
+
+def keys_by_value(dictionary, value=1):
+    res = {}
+    for word, count in dictionary.items():
+        if count == value:
+            res[word] = count
+
+    return res
+
+
+def replace_dict_by_unknown(dictionary, value=1):
+    res = {}
+    for word in dictionary.keys():
+        res[word] = 'UNK'
+
+    return res
+
+
+def change_files_with_unknown_words(parent_path, write_path, unknown_dictionary):
+    read_files = glob.glob(parent_path + '*')
+
+    for file in read_files:
+        with open(file, "r") as infile:
+            file_name = os.path.basename(infile.name)
+            text = infile.read()
+
+        for word in unknown_dictionary.keys():
+            if text.find(word) >= 0:
+                text = keymap_replace(text, unknown_dictionary)
+
+        with open(write_path + file_name, 'w') as out:
+            out.write(text)
+
+
+def keymap_replace(
+    string: str,
+    mappings: dict,
+    lower_keys=False,
+    lower_values=False,
+    lower_string=False,
+) -> str:
+    """Replace parts of a string based on a dictionary.
+
+    This function takes a string a dictionary of
+    replacement mappings. For example, if I supplied
+    the string "Hello world.", and the mappings 
+    {"H": "J", ".": "!"}, it would return "Jello world!".
+
+    Keyword arguments:
+    string       -- The string to replace characters in.
+    mappings     -- A dictionary of replacement mappings.
+    lower_keys   -- Whether or not to lower the keys in mappings.
+    lower_values -- Whether or not to lower the values in mappings.
+    lower_string -- Whether or not to lower the input string.
+    """
+    replaced_string = string.lower() if lower_string else string
+    for character, replacement in mappings.items():
+        replaced_string = replaced_string.replace(
+            character.lower() if lower_keys else character,
+            replacement.lower() if lower_values else replacement
+        )
+    return replaced_string
--- main.py
+++ main.py
@@ -2,27 +2,42 @@
 # from helpers.extensions import load_multiple_corpus_files
 from nltk.corpus.reader.tagged import TaggedCorpusReader
 import nltk
-import json
-from helpers.extensions import lower_files
+from helpers.extensions import count_keys_by_value, lower_files, sort_dict_by_value, keys_by_value, change_files_with_unknown_words, replace_dict_by_unknown
 
 # %%
-lower_files('docs\\brown_hw\\Train\\', 'docs\\brown_hw_lowercase\\Train\\')
-lower_files('docs\\brown_hw\\Test\\', 'docs\\brown_hw_lowercase\\Test\\')
+# lower_files('docs\\brown_hw\\Train\\', 'docs\\brown_hw_lowercase\\Train\\')
+# lower_files('docs\\brown_hw\\Test\\', 'docs\\brown_hw_lowercase\\Test\\')
 
 # load_multiple_corpus_files('docs\\brown_hw\\Train\\')
 # load_multiple_corpus_files('docs\\brown_hw\\Test\\')
 
 # %%
-train_root = 'docs\\brown_hw\\Train'
+train_root = 'docs\\brown_hw_lowercase\\Train'
 train_reader = TaggedCorpusReader(train_root, '.*')
 # %%
 train_words = train_reader.words()
 # %%
 train_word_counts = nltk.FreqDist(train_words)
 # %%
-with open('docs/train_word_counts.json', 'w') as fp:
-    json.dump(train_word_counts, fp)
+# with open('docs/train_word_counts.json', 'w') as fp:
+#     json.dump(train_word_counts, fp)
 # %%
-lower_all_files('docs\\brown_hw\\Train')
+# train_word_counts = sort_dict_by_value(train_word_counts)
 
+# %%
+train_word_counts_1 = keys_by_value(train_word_counts)
+
+# %%
+unknown_dictionary = replace_dict_by_unknown(train_word_counts_1)
+# %%
+change_files_with_unknown_words(
+    'docs\\brown_hw_lowercase\\Train\\', 'docs\\brown_hw_with_unk\\Train\\', unknown_dictionary)
+# %%
+train_root = 'docs\\brown_hw_with_unk\\Train'
+train_reader = TaggedCorpusReader(train_root, '.*')
+# %%
+train_words = train_reader.words()
+
+# %%
+train_words
 # %%
